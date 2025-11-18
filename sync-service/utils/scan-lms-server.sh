8124a01b12afdaa149ac60d81d85ba6815aae40a
wooge <imgburn_carli@simplelogin.com>
2025-11-18 22:14:16 +0100

    added remaining parameters including api-version

--- sync-service/utils/scan-lms-server.sh
+++ sync-service/utils/scan-lms-server.sh
@@ -1,3 +1,3 @@
 echo "Requesting LMS server scan..."
-response=$(curl -s "http://host.docker.internal:4533/rest/startScan?apiKey=$LMS_API_KEY")
+response=$(curl -s "http://host.docker.internal:4533/rest/startScan?apiKey=$LMS_API_KEY&c=sync-service&v=$LMS_API_VERSION")
 echo "Response: $response"
