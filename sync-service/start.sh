7b17fba098378e20a7a01aa75ab0fe8560c2e703
wooge <imgburn_carli@simplelogin.com>
2025-11-20 21:16:48 +0100

    fixing lineendings in playlists.txt

--- sync-service/start.sh
+++ sync-service/start.sh
@@ -7,5 +7,11 @@ echo "Running sync-service"
 # Cronjob triggering scheduled processes
 cron -f &
 
+# Fixing line-endings in playlists.txt
+tmpfile=$(mktemp)
+tr -d '\r' < /playlists.txt > "$tmpfile"
+cat "$tmpfile" > /playlists.txt
+rm "$tmpfile"
+
 # Start socat in the foreground, listening to outside triggers
 socat TCP-LISTEN:8080,fork SYSTEM:"/scripts/update-library-listen.sh"
