da8cb87d4ddcf8b654037ffaf9670181647d7eca
wooge <imgburn_carli@simplelogin.com>
2025-11-30 09:34:44 +0100

    fixed typo and deployment of prod compose

--- deploy-to-registry.sh
+++ deploy-to-registry.sh
@@ -1,3 +1,5 @@
+export COMPOSE_FILE="docker-compose.yaml:docker-compose.prod.yaml"
+
 docker compose down --rmi local --remove-orphans
 docker compose build
 docker compose push
--- launch-prod.bat
+++ launch-prod.bat
@@ -1,3 +1,3 @@
 docker compose down --rmi local --remove-orphans
 docker compose pull
-docker compose -f docker-compose.yaml -f docker-compose.prod.yml up -d --no-build
+docker compose -f docker-compose.yaml -f docker-compose.prod.yaml up -d --no-build
