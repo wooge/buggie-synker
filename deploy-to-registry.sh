ca35e36d6da1dded10be91bed2cc0767f0a0323d
wooge <imgburn_carli@simplelogin.com>
2025-11-30 09:37:55 +0100

    corrected deploy-to-registry to work on windows

--- deploy-to-registry.sh
+++ deploy-to-registry.sh
@@ -1,4 +1,5 @@
-export COMPOSE_FILE="docker-compose.yaml:docker-compose.prod.yaml"
+# Semicolon separating because running on Windows. Replace with colon on Linux/macOS.
+export COMPOSE_FILE="docker-compose.yaml;docker-compose.prod.yaml"
 
 docker compose down --rmi local --remove-orphans
 docker compose build
